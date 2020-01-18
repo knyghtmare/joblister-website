41aeaadada2d59feb00edce3c73d1da4c6731641
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-18 21:19:34 +0600

    Update Job.php

--- lib/Job.php
+++ lib/Job.php
@@ -53,6 +53,77 @@ class Job
 
         return $this->db->single();
     }
+
+    public function create(array $data): bool
+    {
+        $this->db->query("INSERT INTO jobs(category_id, job_title, company,
+                  description, location, salary, contact_user, contact_email) VALUES
+                  (:category_id, :job_title, :company,
+                  :description, :location, :salary, :contact_user, :contact_email)");
+
+        // Bind Data
+        $this->db->bind(':category_id', $data['category_id']);
+        $this->db->bind(':job_title', $data['job_title']);
+        $this->db->bind(':company', $data['company']);
+        $this->db->bind(':description', $data['description']);
+        $this->db->bind(':location', $data['location']);
+        $this->db->bind(':salary', $data['salary']);
+        $this->db->bind(':contact_user', $data['contact_user']);
+        $this->db->bind(':contact_email', $data['contact_email']);
+
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function update(string $id, array $data): bool
+    {
+        $this->db->query("UPDATE jobs SET 
+                  category_id = :category_id, 
+                  job_title = :job_title, 
+                  company = :company,
+                  description = :description, 
+                  location = :location, 
+                  salary = :salary, 
+                  contact_user = :contact_user, 
+                  contact_email = :contact_email
+                  WHERE id = :id");
+
+        // Bind Data
+        $this->db->bind(':category_id', $data['category_id']);
+        $this->db->bind(':job_title', $data['job_title']);
+        $this->db->bind(':company', $data['company']);
+        $this->db->bind(':description', $data['description']);
+        $this->db->bind(':location', $data['location']);
+        $this->db->bind(':salary', $data['salary']);
+        $this->db->bind(':contact_user', $data['contact_user']);
+        $this->db->bind(':contact_email', $data['contact_email']);
+        $this->db->bind(':id', $id);
+
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete(string $id): bool
+    {
+        $this->db->query("DELETE FROM jobs WHERE id = :id");
+
+        $this->db->bind(':id', $id);
+
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
  ?>
